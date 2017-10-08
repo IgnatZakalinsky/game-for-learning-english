4204dfbd2f8f69c41c7329f1421ea7883e49e1a4
IgnatZakalinsky <ai73a@yandex.ru>
2017-10-08 17:36:04 +0200

    add load serv

--- src/Server/Accs.java
+++ src/Server/Accs.java
@@ -1,10 +1,52 @@
 package Server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
 final class Accs {
-    //list
+    TreeMap<String, ArrayList<String>> accs = new TreeMap<>();
+    TreeMap<String, Socket> acc = new TreeMap<>();
+    TreeMap<String, ArrayList<String>> accOn = new TreeMap<>();
 
     Accs() {
         //read file
+        LinkedList<Byte> b = new LinkedList<>();
+        FileInputStream fin;
+        try {
+            fin = new FileInputStream("src\\Server\\accs.txt");
+            do {
+                b.add((byte) fin.read()); // побайтовое чтение
+            } while (b.getLast() != -1);
+            fin.close();
+        } catch (IOException e) { e.printStackTrace(); }
+        b.removeLast();
+        byte[] b2 = new byte[b.size()];
+        int i = 0, j = 1;
+        for (byte b3 : b) {
+            b2[i++] = b3;
+        }
+        String st = new String(b2);
+        String[] ss = st.split("\r\n");
+        for (String s0 : ss) System.out.println("[" + s0 + "]");
+
+        try {
+            i = Integer.parseInt(ss[0]);
+            for (int ac = 0; ac < i; ac++) {
+                ArrayList<String> ap = new ArrayList<>();
+                for (int p = 0; p < 3; p++) {
+                    ap.add(ss[j++]);
+                    System.out.print(":" + ss[j - 1]);
+                }
+                accs.put(ap.get(0), ap);
+                System.out.println(":acc:" + ac);
+            }
+        } catch (Exception e) { System.out.println(e + "\nfile accs.txt is corrupted"); }
+
+
     }
 
 }
