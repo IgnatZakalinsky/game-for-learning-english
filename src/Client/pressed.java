691125065532202e6b36112c67f80f0b33be2a4c
IgnatZakalinsky <ai73a@yandex.ru>
2017-10-11 21:54:46 +0200

    working control

--- src/Client/pressed.java
+++ src/Client/pressed.java
@@ -7,7 +7,7 @@ public class pressed implements Runnable {
     final int STEP = 5;
     Thread t;
     GameWindow gw;
-    volatile char e = '0';
+    volatile boolean ex = false, ey = false, e_y = false, e_x = false;
     boolean q = true;
 
     pressed(GameWindow gw) {
@@ -19,10 +19,10 @@ public class pressed implements Runnable {
     @Override
     public void run() {
         while (q) {
-            if (e == 'w') GameWindow.y += STEP;
-            if (e == 's') GameWindow.y -= STEP;
-            if (e == 'a') GameWindow.x += STEP;
-            if (e == 'd') GameWindow.x -= STEP;
+            if (ey) GameWindow.y += STEP;
+            if (e_y) GameWindow.y -= STEP;
+            if (ex) GameWindow.x += STEP;
+            if (e_x) GameWindow.x -= STEP;
 
             try {
                 Thread.sleep(50);
@@ -33,8 +33,17 @@ public class pressed implements Runnable {
         //q = true;
     }
 
-    public void setE(char e, boolean p) {
-        if (p) this.e = e;
-        else if (this.e == e) this.e = '0';
+    void setE(char e, boolean p) {
+        if (p) {
+            if (e == 'w') ey = true;
+            else if (e == 's') e_y = true;
+            else if (e == 'a') ex = true;
+            else if (e == 'd') e_x = true;
+        } else {
+            if (e == 'w') ey = false;
+            else if (e == 's') e_y = false;
+            else if (e == 'a') ex = false;
+            else if (e == 'd') e_x = false;
+        }
     }
 }
