536102da06ac3230ec18aec529429e8a681e3dea
IgnatZakalinsky <ai73a@yandex.ru>
2017-10-11 20:38:10 +0200

    add paint in gameWindow

--- src/Client/GameWindow.java
+++ src/Client/GameWindow.java
@@ -1,11 +1,18 @@
 package Client;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameWindow extends Frame implements WindowListener, Runnable {
+    int x = -300, y = -300;
+    boolean q = true;
     Thread t;
+    volatile private static BufferedImage b2 = new BufferedImage(800, 600, 1);
 
     GameWindow() {
         addWindowListener(this);
@@ -13,8 +20,8 @@ public class GameWindow extends Frame implements WindowListener, Runnable {
         setTitle("Game");
         setBackground(Color.BLACK);
 
-        setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2 - 400,
-                Toolkit.getDefaultToolkit().getScreenSize().height/2 - 300); // по центру
+        setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - 400,
+                Toolkit.getDefaultToolkit().getScreenSize().height / 2 - 300); // по центру
         setResizable(false); // не изменять размеры
 
         setVisible(true);
@@ -62,6 +69,44 @@ public class GameWindow extends Frame implements WindowListener, Runnable {
 
     @Override
     public void run() {
+        go();
+    }
 
+    private void go() {
+        while (q) {
+            //temp const
+            BufferedImage bi = null, gg = null;
+            try {
+                bi = ImageIO.read(new File("src\\Client\\local1.jpg"));
+                gg = ImageIO.read(new File("src\\Client\\gg.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Graphics2D g2d;
+            g2d = (Graphics2D) b2.getGraphics();
+
+            //g2d.rotate(Math.PI / 50 * s.a, s.x + 75, s.y + 75);
+            g2d.drawImage(bi, x, y, 1300, 1300, null);
+            g2d.drawImage(gg, 370, 270, 60, 60, null);
+
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            repaint();
+        }
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        update(g);
+    }
+
+    @Override
+    public void update(Graphics g) {
+        g.drawImage(b2, 0, 0, this);
     }
 }
---
+++ src/Client/Key.java
@@ -0,0 +1,21 @@
+package Client;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Key implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
